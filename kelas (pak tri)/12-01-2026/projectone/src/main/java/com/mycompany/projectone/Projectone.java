6f73eda4594850ca82fc4366eec0887b4d1777d2
kuhaku0513 <kuhaku0513@gmail.com>
2026-01-12 09:41:33 +0700

    Add files via upload

---
+++ kelas (pak tri)/12-01-2026/projectone/src/main/java/com/mycompany/projectone/Projectone.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projectone;
+
+/**
+ *
+ * @author MyPC PRO
+ */
+public class Projectone {
+
+    public static void main(String[] args) {
+     int angka;
+         angka = 10;
+     int pk;
+         pk = 2;
+     int pb; 
+         pb = 5; 
+         
+        System.out.println("angka");
+        System.out.println("angka yang anda inputkan : "+angka);
+        System.out.println("hasil kombinasi perkalian anda : "+angka*pk);
+        System.out.println("hasil kombinasi pmbagian anda : "+angka/pb);
+        System.out.println("hasil kombinasi ketiganya adalah : "+angka*pk/pb);
+        angka = 100;
+        pk = 15;
+        System.out.println("angka terbaru adalah : "+angka);
+        
+     double chain;
+            chain = 2;
+        System.out.println(chain);  
+        
+     float decimal;
+           decimal = 1.23456789f;
+           System.out.println(decimal);
+           
+    String pantun;
+           pantun = "ikan asin makan tomat";
+           System.out.println(pantun);
+                 
+    char a;
+         a = '+';
+         System.out.println("a");  
+         
+         
+         
+    boolean benar = true;
+    boolean salah = false;   
+            System.out.println(benar);
+            System.out.println("salah");
+    }
+}
